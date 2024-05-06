f0f365e9c909f18bbcfb0735d1ba7e4dd52d4473
Axel Marmoret <axel.marmoret@imt-atlantique.fr>
2024-05-06 17:40:54 +0200

    Pattern study was fused into factorisation to signal, audio helper, and spectrogram to signal. Cleaner code, but way result in a bug (not tested extensively)

--- barmuscomp/model/errors.py
+++ barmuscomp/model/errors.py
@@ -9,3 +9,12 @@ Module defining specific errors to be raised
 
 # %% Load everything from as_seg
 from as_seg.model.errors import *
+
+
+class UndesiredScenarioException(BaseException): pass
+class MaskAlmostOneException(UndesiredScenarioException): pass
+class PatternNeverUsedException(UndesiredScenarioException): pass
+
+class NotImplementedException(BaseException): pass
+
+
