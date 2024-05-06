3018d5407e93d796e90a794e5957c00a6509238f
Axel Marmoret <axel.marmoret@imt-atlantique.fr>
2024-05-06 17:42:11 +0200

    Updating the init .py

--- barmuscomp/__init__.py
+++ barmuscomp/__init__.py
@@ -3,11 +3,13 @@ from . import ae_ntd
 from . import ae_utils
 from . import lra
 
+from .model import audio_helper
 from .model import current_plot
 from .model import early_stopping
 from .model import errors
-from .model import features
-from .model import pattern_study
+from .model import factorisation_to_signal
+from .model import signal_to_spectrogram
+from .model import spectrogram_to_signal
 
 from .scripts import default_path
 from .scripts import overall_scripts
