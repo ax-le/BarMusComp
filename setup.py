6cad609dc0f00466409d929d50a0e86972bbf25c
Axel Marmoret <axel.marmoret@imt-atlantique.fr>
2024-05-06 17:44:09 +0200

    Up-to-date with Pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="barmuscomp",
-    version="0.1.4",
+    version="0.1.5",
     author="Marmoret Axel",
     author_email="axel.marmoret@irisa.fr",
     description="Package for barwise compression applied on musical segmentation.",
